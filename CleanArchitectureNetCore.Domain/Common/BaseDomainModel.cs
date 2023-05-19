df4276df080f4d9af1a5f067c58919c81b874181
Leonardo <luisfonseca9521@gmail.com>
2023-05-18 22:22:41 -0600

    Base domain

---
+++ CleanArchitectureNetCore.Domain/Common/BaseDomainModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitectureNetCore.Domain.Common
+{
+  public abstract class BaseDomainModel
+  {
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+  }
+}
