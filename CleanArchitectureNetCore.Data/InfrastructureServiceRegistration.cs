cd6be89e3640154aa675810c990f66893e7e8277
Leonardo <luisfonseca9521@gmail.com>
2023-05-20 19:29:39 -0600

    Creacion de contenedor de inyeccion de dependencias

---
+++ CleanArchitectureNetCore.Data/InfrastructureServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitectureNetCore.Application.Contracts.Infrastucture;
+using CleanArchitectureNetCore.Application.Contracts.Persistence;
+using CleanArchitectureNetCore.Application.Models;
+using CleanArchitectureNetCore.Infrastruture.Persistence.Context;
+using CleanArchitectureNetCore.Infrastruture.Repositories;
+using CleanArchitectureNetCore.Infrastruture.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureNetCore.Infrastruture
+{
+  public static class InfrastructureServiceRegistration
+  {
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+      services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+      services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+      services.AddScoped(typeof(IVideoRepository), typeof(VideoRepository));
+      services.AddScoped(typeof(IStreamerRepository), typeof(StreamerRepository));
+
+      services.Configure<EmailsSettings>(options => configuration.GetSection("EmailSettings"));
+      services.AddTransient(typeof(IEmailService), typeof(EmailService));
+
+      return services;
+    }
+  }
+}
